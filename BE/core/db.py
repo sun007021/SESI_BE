d8b8b5eadbfd0ca36dce075ea1aa7322e8309a5a
Sunwook Lee <85978148+sun007021@users.noreply.github.com>
2024-07-30 11:25:55 +0000

    style: delete comments

--- BE/core/db.py
+++ BE/core/db.py
@@ -10,17 +10,11 @@ engine = create_async_engine(
     'postgresql+asyncpg://sesiadmin:sesi1234@localhost:5432/sesidb')
 
 
-# TODO: 비동기 제대로 사용하려면 alembic 사용 필요
 # 세션 생성기
 SessionLocal = async_sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-# 종속성 주입을 위한 세션 생성 함수
-# @contextlib.asynccontextmanager
-# NOTE: 동기방식에서는 endpoint에서 Depends로 get_db() 함수 이용시 데코레이터 사용 X 사용시 중복되어 오류
-# 하지만 비동기 방식에는 빼야하는지 넣어야하는지 몰라서 일단 주석처리함
 
 
 async def get_db():
